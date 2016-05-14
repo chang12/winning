11c1b9c2c8c125ec121d7b36b1b396b1011f213b
Changhyun Lee <chang12@snu.ac.kr>
2016-05-14 17:09:43 +0900

    Add: record App에 Match 모델 생성

--- record/models.py
+++ record/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Match(models.Model):
+    player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player1')
+    player2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player2')
+    team1 = models.CharField(max_length=20)
+    team2 = models.CharField(max_length=20)
+    time = models.DateTimeField(auto_now_add=True)
+    score1 = models.IntegerField()
+    score2 = models.IntegerField()
--- winning/settings.py
+++ winning/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
-    'winning',
+    'record',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
