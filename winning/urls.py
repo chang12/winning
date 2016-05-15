d2abaf83215eb5c61e5cfe827257fbd07f7a39c0
Changhyun Lee <chang12@snu.ac.kr>
2016-05-16 00:23:19 +0900

    Add: 로그인/로그아웃 기본 구현

--- winning/urls.py
+++ winning/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^record/', include('record.urls', namespace='record')),
 ]
