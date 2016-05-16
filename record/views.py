3ffdd3bfaced943cc09ea0354a36111421511444
Changhyun Lee <chang12@snu.ac.kr>
2016-05-16 17:25:42 +0900

    Add: 상대 전적 승/무/패 기본 구현

--- record/views.py
+++ record/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 from .forms import MatchForm, RivalForm
+from .models import Match
 
 # Create your views here.
 def index(request):
@@ -47,9 +48,21 @@ def detail(request, pk1, pk2):
     if request.user.pk == int(pk1):
         p1 = User.objects.get(pk=pk1)
         p2 = User.objects.get(pk=pk2)
+        matches = Match.objects.filter(player1=p1, player2=p2)
+        win, draw, defeat = 0, 0, 0
+        for match in matches:
+            if match.score1 > match.score2:
+                win = win + 1;
+            elif match.score1 < match.score2:
+                defeat = defeat + 1;
+            else:
+                draw = draw + 1;
         return render(request, 'record/detail.html', {
             'name1': p1.username,
             'name2': p2.username,
+            'win': win,
+            'draw': draw,
+            'defeat': defeat,
         })
     else:
         msg = "자신이 경기한 전적만 열람할 수 있습니다."
