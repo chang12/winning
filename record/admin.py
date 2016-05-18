d8e77a0bf7a1e49e9424de0ec8e59553d55e61e4
Changhyun Lee <chang12@snu.ac.kr>
2016-05-18 12:46:32 +0900

    Modify: Match의 team1, team2 field를 ForeignKey로 변경

--- record/admin.py
+++ record/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Match
+from .models import Match, Team
 
 # Register your models here.
 admin.site.register(Match)
+admin.site.register(Team)
--- record/models.py
+++ record/models.py
@@ -2,11 +2,17 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    teamname = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.teamname
+
 class Match(models.Model):
     player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player1')
     player2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player2')
-    team1 = models.CharField(max_length=20)
-    team2 = models.CharField(max_length=20)
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team1')
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team2')
     time = models.DateTimeField(auto_now_add=True)
     score1 = models.IntegerField()
     score2 = models.IntegerField()
@@ -16,6 +22,3 @@ class Match(models.Model):
 
 class Rival(models.Model):
     player = models.ForeignKey(User)
-
-class Team(models.Model):
-    teamname = models.CharField(max_length=20)
