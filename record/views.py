13ad9f7b01645d27651ac819eaf7c086bf6e7738
Changhyun Lee <chang12@snu.ac.kr>
2016-05-21 00:14:00 +0900

    Modify: 상대방이 나와의 전적 입력한것 안보이는 오류 수정

--- record/views.py
+++ record/views.py
@@ -19,31 +19,32 @@ def index(request):
 @login_required
 def match_new(request):
     if request.method == 'POST':
-        teams = ''
         form = MatchForm(request.POST)
         if form.is_valid():
             model = form.save(commit=False)
-            model.player1 = request.user
-            pk = int(request.GET['rival'])
-            rival = User.objects.filter(pk=pk)
-            model.player2 = User.objects.get(pk=pk)
-            team1 = Team.objects.get(pk=request.POST['team1'])
-            team2 = Team.objects.get(pk=request.POST['team2'])
-            model.team1, model.team2 = team1, team2
+            player1, player2 = request.user, User.objects.get(pk=int(request.GET['rival']))
+            team1, team2 = Team.objects.get(pk=request.POST['team1']), Team.objects.get(pk=request.POST['team2'])
+            # model에 player를 입력할때는 pk가 작은게 1, pk가 큰게 2로 넣는다.
+            if player1.pk < player2.pk:
+                model.player1, model.player2 = player1, player2
+                model.team1, model.team2 = team1, team2
+            else:
+                model.player1, model.player2 = player2, player1
+                model.team1, model.team2 = team2, team1
+                # 점수도 뒤집어줘야 한다.
+                model.score1, model.score2 = model.score2, model.score1
             model.save()
         return redirect('record:index')
     else:
         form = MatchForm()
         teams = Team.objects.all()
-
-    return render(request, 'record/match_new.html', {
-        'form': form,
-        'teams': teams,
-    })
+        return render(request, 'record/match_new.html', {
+            'form': form,
+            'teams': teams,
+        })
 
 @login_required
 def find(request):
-    users = ''
     if request.method == 'POST':
         flag = int(request.GET['flag'])
         pk1 = request.user.pk
@@ -58,22 +59,26 @@ def find(request):
             return redirect('record:detail', pk1, pk2)
     else:
         form = RivalForm()
-        pk = int(request.user.pk)
+        pk = request.user.pk
         users = User.objects.filter(~Q(pk = pk))
-
-    return render(request, 'record/rival.html', {
-        'form': form,
-        'users': users,
-    })
+        return render(request, 'record/rival.html', {
+            'form': form,
+            'users': users,
+        })
 
 
 @login_required
 def detail(request, pk1, pk2):
     if request.user.pk == int(pk1):
         p1, p2 = User.objects.get(pk=pk1), User.objects.get(pk=pk2)
-        matches = Match.objects.filter(player1=p1, player2=p2).order_by('-time')
-        win, draw, defeat, GF, GA = 0, 0, 0, 0, 0
 
+        # pk 값이 작은쪽이 늘 player1으로 만들어져있을 것이다.
+        if pk1 < pk2:
+            matches = Match.objects.filter(player1=p1, player2=p2).order_by('-time')
+        else:
+            matches = Match.objects.filter(player1=p2, player2=p1).order_by('-time')
+
+        win, draw, defeat, GF, GA = 0, 0, 0, 0, 0
         for match in matches:
             GF = GF + match.score1
             GA = GA + match.score2
@@ -83,36 +88,65 @@ def detail(request, pk1, pk2):
                 defeat = defeat + 1;
             else:
                 draw = draw + 1;
+        # pk1이 더 크다면, 뒤집어줄 것을 뒤집어준다.
+        if pk1 > pk2:
+            win, defeat, GF, GA = defeat, win, GA, GF
 
         last_five = matches[:5]
         curr_five = ""
-        for match in last_five:
-            if match.score1 > match.score2:
-                curr_five = '승' + curr_five
-            elif match.score1 < match.score2:
-                curr_five = '패' + curr_five
-            else:
-                curr_five = '무' + curr_five
+        if pk1 < pk2:
+            for match in last_five:
+                if match.score1 > match.score2:
+                    curr_five = '승' + curr_five
+                elif match.score1 < match.score2:
+                    curr_five = '패' + curr_five
+                else:
+                    curr_five = '무' + curr_five
+        else:
+            for match in last_five:
+                if match.score1 > match.score2:
+                    curr_five = '패' + curr_five
+                elif match.score1 < match.score2:
+                    curr_five = '승' + curr_five
+                else:
+                    curr_five = '무' + curr_five
 
-        team1s = Match.objects.filter(player1=p1, player2=p2).values('team1').distinct()
-        infos = []
-        for team1 in team1s:
-            team = Team.objects.get(pk=team1['team1'])
-            matches = Match.objects.filter(player1=p1, player2=p2, team1=team)
-            if len(matches) >= 3:
-                twin, tdraw, tdefeat = 0, 0, 0
-                for m in matches:
-                    twin = twin + (m.score1 > m.score2)
-                    tdraw = tdraw + (m.score1 == m.score2)
-                    tdefeat = tdefeat + (m.score1 < m.score2)
-                point = (twin * 3 + tdraw) / len(matches)
-                infos.append({"teamname": team.teamname, "point": point})
-        # point 내림차순으로 정렬.
-        infos.sort(key=getkey, reverse=True)
+        if pk1 < pk2:
+            team1s = Match.objects.filter(player1=p1, player2=p2).values('team1').distinct()
+            infos = []
+            for team1 in team1s:
+                team = Team.objects.get(pk=team1['team1'])
+                matches = Match.objects.filter(player1=p1, player2=p2, team1=team)
+                if len(matches) >= 3:
+                    twin, tdraw, tdefeat = 0, 0, 0
+                    for m in matches:
+                        twin = twin + (m.score1 > m.score2)
+                        tdraw = tdraw + (m.score1 == m.score2)
+                        tdefeat = tdefeat + (m.score1 < m.score2)
+                    point = (twin * 3 + tdraw) / len(matches)
+                    infos.append({"teamname": team.teamname, "point": point})
+            # point 내림차순으로 정렬.
+            infos.sort(key=getkey, reverse=True)
+        else:
+            team2s = Match.objects.filter(player1=p2, player2=p1).values('team2').distinct()
+            infos = []
+            for team2 in team2s:
+                team = Team.objects.get(pk=team2['team2'])
+                matches = Match.objects.filter(player1=p2, player2=p1, team2=team)
+                if len(matches) >= 3:
+                    twin, tdraw, tdefeat = 0, 0, 0
+                    for m in matches:
+                        twin = twin + (m.score1 < m.score2)
+                        tdraw = tdraw + (m.score1 == m.score2)
+                        tdefeat = tdefeat + (m.score1 > m.score2)
+                    point = (twin * 3 + tdraw) / len(matches)
+                    infos.append({"teamname": team.teamname, "point": point})
+            # point 내림차순으로 정렬.
+            infos.sort(key=getkey, reverse=True)
 
         return render(request, 'record/detail.html', {
-            'name1': p1.profile.nickname,
-            'name2': p2.profile.nickname,
+            'p1': p1,
+            'p2': p2,
             'win': win,
             'draw': draw,
             'defeat': defeat,
@@ -121,6 +155,7 @@ def detail(request, pk1, pk2):
             'curr_five': curr_five,
             'last_five': last_five,
             'infos': infos,
+            'pk1_is_smaller': (p1.pk < p2.pk),
         })
     else:
         msg = "자신이 경기한 전적만 열람할 수 있습니다."
