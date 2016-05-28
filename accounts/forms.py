33586305442953ec8261878c87e81e11aadabe18
Changhyun Lee <chang12@snu.ac.kr>
2016-05-28 18:14:33 +0900

    Add: 헤더/풋터 추가하고 메뉴버튼 기본구현 완료

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,17 +5,18 @@ from .models import Profile
 
 
 class MyUserCreationForm(UserCreationForm):
-    nickname = forms.CharField(min_length=1, max_length=20)
+
+    # self.fields['username'].help_text=None
 
     def save(self, commit=True):
-        # 다중 상속시에 상위 클래스들의 메소드 중복 호출을 피하기 위해 super 가 사용됨.
+        # 다중 상속시에 상위 클래스들의 메소드 중복 호출을 피하기 위해 super가 사용됨.
         # 인자로 class 이름과 self를 왜 받는지에 대해서는 모름.
         user = super(MyUserCreationForm, self).save(commit=False)
         if commit:
             user.is_active = True
             user.save()
 
-        profile = Profile(user=user, nickname=self.cleaned_data['nickname'])
+        profile = Profile(user=user)
         profile.save()
         user.profile = profile
         user.save()
@@ -23,5 +24,12 @@ class MyUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(MyUserCreationForm, self).__init__(*args, **kwargs)
+        # 각 input 태그의 help_text, label을 제거함.
+        for field in self.fields:
+            self.fields[field].help_text=None
+            self.fields[field].label=''
+        # 각 input 태그에 placeholder를 추가함.
+        self.fields['username'].widget.attrs['placeholder'] = "이름"
+        self.fields['password1'].widget.attrs['placeholder'] = "비밀번호"
+        self.fields['password2'].widget.attrs['placeholder'] = "비밀번호 확인"
 
-        # UserCreationForm의 각 field 들의 help text를 없애는 코드부분이 있지만 (아마 근우가 쓴 듯), 우선은 생략함.
--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=20)
+    # nickname = models.CharField(max_length=20)
