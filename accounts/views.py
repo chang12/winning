a8ec6dad74a65a22cdaddce04f6c652483a639a0
Changhyun Lee <chang12@snu.ac.kr>
2016-06-22 02:06:40 +0900

    Modify: UI/UX를 손봄. message를 사용하고, W3School 복붙해서 적당히 꾸며봄.

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+from django.contrib.auth import login as auth_login
 from django.shortcuts import redirect, render
 
 from .forms import MyUserCreationForm
 
+
 # Create your views here.
 def login(request):
-
     auth_login(request)
 
 
@@ -14,6 +15,7 @@ def signup(request):
         form = MyUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.info(request, '인증 이메일이 발송되었습니다.')
             return redirect('record:index')
 
     else:
