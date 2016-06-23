6e84dc6c0a6fe1c9ddb8cf5c457fc1452f1368a8
Changhyun Lee <chang12@snu.ac.kr>
2016-06-23 19:25:44 +0900

    Modify: index template의 AJAX 처리를 매듭지음. error 메시지를 동적으로 띄우는 기능과, 메인 화면으로 redirect 시켜주는 로직을 완성함.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.models import User
-from django.shortcuts import redirect, render
+from django.core.urlresolvers import reverse
+from django.http import JsonResponse
+from django.shortcuts import render
 
 # from .forms import MyUserCreationForm as UserCreationForm
 from .forms import MyUserCreationForm as UserCreationForm
@@ -23,9 +25,21 @@ def signup(request):
 
             User.objects.create_user(username, email, password)
             messages.info(request, '인증 이메일이 발송되었습니다.')
+            data = {}
+            data['status'] = True
+            data['url'] = reverse('record:index')
 
-            return redirect('record:index')
-
+            return JsonResponse(data)
+        else:
+            # 적어도 하나의 error는 존재할 것이다.
+            # error가 존재하는 field 중 하나를 임의로 받아온다.
+            error_key = list(form.errors.keys())[0]
+            # 그 field의 첫번째 error의 메시지를 획득한다.
+            error_message = form.errors[error_key][0]
+            data = {}
+            data['status'] = False
+            data['error'] = error_message
+            return JsonResponse(data)
     else:
         form = UserCreationForm()
 
